c75874b42ed707caa78a81a08835b2964630003a
chanuka-dsc <chanuka.dsc@gmail.com>
2021-10-26 18:09:02 +0530

    header compleated

--- src/App.js
+++ src/App.js
@@ -2,10 +2,9 @@ import Header from "./Components/Layout/Header";
 
 function App() {
   return (
-    <div>
-      <h2>Let's get started!</h2>
+    <>
       <Header />
-    </div>
+    </>
   );
 }
 
--- src/Components/Layout/Header.js
+++ src/Components/Layout/Header.js
@@ -1,13 +1,17 @@
+import image from "../../Assests/meals.jpeg";
 import classes from "./Header.module.css";
+import HeaderCardButton from "./HeaderCardButton";
 
 const Header = () => {
   return (
     <>
       <header class={classes.header}>
         <h1>React Meals </h1>
-        <button>Cart</button>
+        <HeaderCardButton />
       </header>
-      <div>{/* <img src={meals} alt="some_image" /> */}</div>
+      <div className={classes["main-image"]}>
+        <img src={image} alt="some_image" />
+      </div>
     </>
   );
 };
